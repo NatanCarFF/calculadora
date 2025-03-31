a37d5bd35fdaaed78b3a515184dce38c77e521c9
Natanael Carvalho Da Silva <161735922+NatanCarFF@users.noreply.github.com>
2025-03-31 15:03:15 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/calculadora/', // Replace 'calculator' with your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
