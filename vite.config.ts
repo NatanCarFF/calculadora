3a9ba40efac7bc3a2400a5256d7862095319c030
Natanael Carvalho Da Silva <161735922+NatanCarFF@users.noreply.github.com>
2025-03-31 14:48:52 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/NatanCarFF/', // Replace 'calculator' with your repository name
+  base: '/calculadora/', // Replace 'calculator' with your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
